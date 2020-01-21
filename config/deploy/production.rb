686bdb671ac960b7b5f30b99213432019813ed59
totor <toto@toot.fr>
2020-01-21 23:03:00 +0100

    blabla

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,8 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
- server "34.77.181.91", user: "deathqt1", roles: %w{app db web}
+# server "34.77.181.91", user: "deathqt1", roles: %w{app db web}
+server "34.77.181.91", user: "deployer-agent", roles: %w{app db web}
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
@@ -17,7 +18,8 @@
 # property set. Specify the username and a domain or IP for the server.
 # Don't use `:all`, it's a meta role.
 
-# role :app, %w{deploy@example.com}, my_property: :my_value
+# role :app, %w{Marjorie-ED@34.77.181.91}
+# role :web, %w{deathqt1@34.77.181.91}
 # role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
 # role :db,  %w{deploy@example.com}
 
@@ -41,11 +43,12 @@
 #
 # Global options
 # --------------
-#  set :ssh_options, {
-#    keys: %w(/home/rlisowski/.ssh/id_rsa),
-#    forward_agent: false,
-#    auth_methods: %w(password)
-#  }
+ set :ssh_options, {
+   keys: %w(/c/Users/Marjorie/.ssh/google_compute_engine),
+   forward_agent: false,
+    auth_methods: %w(publickey),
+  #    verbose: :debug
+ }
 #
 # The server-based syntax can be used to override options:
 # ------------------------------------
@@ -54,8 +57,9 @@
 #   roles: %w{web app},
 #   ssh_options: {
 #     user: "user_name", # overrides user setting above
-#     keys: %w(/home/user_name/.ssh/id_rsa),
+#     keys: %w(/home/user_name/.ssh/new),
 #     forward_agent: false,
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
+fetch(:default_env).merge!(wp_env: :production)
