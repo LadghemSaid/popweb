6bec9a53035f41f2278e64d9eac451da7e44f9ee
totor <toto@toot.fr>
2020-03-27 16:19:05 +0100

    test

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -53,6 +53,24 @@ class IndexController extends AbstractController
         ]);
     }
 
+
+    # get success response from recaptcha and return it to controller
+    function captchaverify($recaptcha){
+        $url = "https://www.google.com/recaptcha/api/siteverify";
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, array(
+            "secret"=>"6Levh-QUAAAAAHUOIIv-s_9RbynocJYkKXZ5iE0M","response"=>$recaptcha));
+        $response = curl_exec($ch);
+        curl_close($ch);
+        $data = json_decode($response);
+
+        return $data->success;
+    }
+
     /**
      * @Route("/contact", name="contact.show")
      */
@@ -61,7 +79,7 @@ class IndexController extends AbstractController
         $form = $this->createForm(ContactType::class);
         $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
+        if ($form->isSubmitted() && $form->isValid() && $this->captchaverify($request->get('g-recaptcha-response'))) {
 
             $contactFormData = $form->getData();
             $poolEmail = [
