6815dda76fba895a105dd314e15ca0affc6fc7f9
totor <toto@toot.fr>
2020-03-27 17:06:11 +0100

    test

--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -22,7 +22,7 @@ class ContactType extends AbstractType
             ->add('submit', SubmitType::class, [
                 'label' => 'Envoyer',
                 'attr' => [
-                    'class' => 'btn btn-primary submit g-recaptcha',
+                    'class' => 'btn btn-primary submit',
                 ]
             ])
 //            ->add('captcha' , RecaptchaSubmitType::class,[
